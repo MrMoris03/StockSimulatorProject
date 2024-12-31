e0f5c121e926447990188c722f5e241c61bc1710
MrMoris03 <yusuphmoris03@gmail.com>
2024-12-31 11:41:23 +0300

    Create a stock class

---
+++ StockMarketSimulator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+struct Stock {
+	public:
+		string name;
+		float price;
